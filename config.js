766140e810a992c2362d413210bbcd70861528aa
Nam Jaeyoun <siisee111@gmail.com>
2020-01-30 15:07:57 +0900

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'JY N',
   githubUrl: 'https://github.com/siisee11/medium-contents-blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://siisee11-medium.netlify.com/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
