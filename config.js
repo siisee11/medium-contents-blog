132595c35638c1eba354993d4ceaa1c2455d8974
Nam Jaeyoun <siisee111@gmail.com>
2020-01-30 15:05:23 +0900

    first commit :tada:

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // the name of your website
+  title: 'JY N',
+  // the description of the website (eg. what shows on Google)
+  description: "JY N's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by JY N ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: ``,
+  author: 'JY N',
+  githubUrl: 'https://github.com/siisee11/medium-contents-blog',
+  // replace this by the url where your website will be published
+  siteUrl: 'http://localhost:8000',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '',
+    medium: '@siisee111',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+}
